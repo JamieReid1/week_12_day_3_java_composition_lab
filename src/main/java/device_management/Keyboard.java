954cb1f0102c5fc33cd89635b28fc9b9dfa582ed
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-20 14:55:09 +0000

    Adds Keyboard pressKey method, passing test.

---
+++ src/main/java/device_management/Keyboard.java
@@ -0,0 +1,20 @@
+package device_management;
+
+public class Keyboard {
+
+    String brand;
+    String language;
+
+    public Keyboard(String brand, String language){
+        this.brand = brand;
+        this.language = language;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+}
---
+++ src/test/java/KeyboardTest.java
@@ -0,0 +1,28 @@
+import device_management.Keyboard;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KeyboardTest {
+
+    Keyboard keyboard;
+
+    @Before
+    public void before(){
+
+        keyboard = new Keyboard("Logitech", "English, UK");
+
+    }
+
+    @Test
+    public void hasBrand(){
+        assertEquals("Logitech", keyboard.getBrand());
+    }
+
+    @Test
+    public void hasALanguage(){
+        assertEquals("English, UK", keyboard.getLanguage());
+    }
+
+}
