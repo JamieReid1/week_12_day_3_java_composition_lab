4ea12befff2d354c97cfbe9becf7e718fa42055d
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-20 14:30:57 +0000

    Initialises project.

---
+++ src/main/java/device_management/SoundDevice.java
@@ -0,0 +1,18 @@
+package device_management;
+
+public abstract class SoundDevice {
+
+    int maxVolume;
+
+    public SoundDevice(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMaxVolume() {
+        return this.maxVolume;
+    }
+
+    public String playSound(String sound) {
+        return "playing: " + sound;
+    }
+}
